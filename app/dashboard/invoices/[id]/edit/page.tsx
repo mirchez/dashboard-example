31cdcbf4dcc8102cf04427d9a61a8a7f32a2780a
Miguel <mmirandasanchez16@gmail.com>
2025-04-11 12:00:00 -0300

    Commit del 11 de abril del 2025

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,26 @@
+import Form from "@/app/ui/invoices/edit-form";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]); //REMINDER: When all the promises in the given iterable fulfill. The fulfillment value is an array of fulfillment values, in the order of the promises passed, regardless of completion order.
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
