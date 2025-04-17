ad82d07624970ef0a60a64d139688200a467e3da
Miguel <mmirandasanchez16@gmail.com>
2025-04-17 14:21:00 -0300

    Commit del 17 de abril del 2025 a las 14:21

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -11,9 +11,9 @@ export const metadata: Metadata = {
 export default async function Page({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const { id } = params;
+  const { id } = await params;
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
