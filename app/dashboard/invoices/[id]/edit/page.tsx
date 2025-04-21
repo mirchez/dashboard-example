45fbffb98ccb6fb943301c8ee661d52a6733d6b9
Miguel <mmirandasanchez16@gmail.com>
2025-04-21 09:57:59 -0300

    delete var and option usless

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 };
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const { id } = params;
+  const { id } = await params;
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
